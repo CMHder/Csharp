bfca21f0d46cf0281428f16a6aef2069bc211123
CMHder <1032257817@qq.com>
2019-01-21 18:56:24 +0800

    提交

---
+++ Excel.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace 成绩管理系统
+{
+    partial class Excel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打印输出：";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Excel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 626);
+            this.Controls.Add(this.button1);
+            this.Name = "Excel";
+            this.Text = "打印输出";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Excel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
+using System.Data.SqlClient;
+
+namespace 成绩管理系统
+{
+    public partial class Excel : Form
+    {
+        public Excel()
+        {
+            InitializeComponent();
+        }
+        public bool DataSetToExcel(DataSet dataSet, bool isShowExcle) 
+        {
+            System.Data.DataTable dataTable = dataSet.Tables[0]; 
+            int rowNumber = dataTable.Rows.Count; 
+
+           int rowIndex = 1; 
+           int colIndex = 0; 
+
+
+            if (rowNumber == 0) 
+            { 
+                return false; 
+            } 
+
+            //建立Excel对象 
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application(); 
+            excel.Application.Workbooks.Add(true);    //新建一张excel工作簿
+            excel.Visible = isShowExcle;   //是否打开该Excel文件
+
+            //生成字段名称 
+            foreach (DataColumn col in dataTable.Columns) 
+            { 
+                colIndex++; 
+                excel.Cells[1, colIndex] = col.ColumnName; 
+            } 
+
+            //填充数据 
+           foreach (DataRow row in dataTable.Rows) 
+            { 
+                rowIndex++; 
+               colIndex = 0; 
+               foreach (DataColumn col in dataTable.Columns) 
+               { 
+                    colIndex++; 
+                  excel.Cells[rowIndex, colIndex] = row[col.ColumnName]; 
+               } 
+            } 
+
+           return true; 
+       }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connection = "SERVER=.;DATABASE=MyData;PWD=1234;UID=sa;";
+            SqlConnection conn1 = new SqlConnection(connection);
+            conn1.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from 课程成绩录入", conn1);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataSetToExcel(ds,true);
+        }
+    }
+
+
+
+
+
+
+
+}
